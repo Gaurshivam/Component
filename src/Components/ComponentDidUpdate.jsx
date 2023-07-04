bcfb1dece695f652794c1c733d4a7ca9050b19a2
GaurShivam <80668477+Gaurshivam@users.noreply.github.com>
2023-07-05 01:31:53 +0530

    Add files via upload

---
+++ src/Components/ComponentDidUpdate.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const ComponentDidUpdate = () => {
+  const [state, setstate] = useState("");
+  const [user, setUser] = useState([]);
+
+  const AddData = () => {
+    setUser([...user,state]);
+    
+  }
+  return (
+    <div style={{textAlign:"center"}}>
+      <h2>Q.2:-  ComponentDidUpdate</h2>
+      <input type="text" placeholder='Enter the value....' value={state} onChange={(e)=>setstate(e.target.value)} />
+      <button onClick={AddData}>Add Data</button>
+      {
+        user.map((ele,i)=>{
+          return (
+            <p key={i}>{ele}</p>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default ComponentDidUpdate;
